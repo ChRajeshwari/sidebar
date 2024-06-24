8c37cbbafc7f65634c048d0ccbc5c5b7e9970f60
ChRajeshwari <rajeshwarisomu21@gmail.com>
2024-06-24 13:51:58 +0530

    added sidebar navigator

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const sidebar=document.querySelector(".sidebar"),
+    onE1=document.querySelector(".on"),
+    searchLi=document.querySelector(".searchli"),
+    modeSwitch=document.querySelector(".mode"),
+    modeText=document.querySelector(".dark");
+
+ 
+    onE1.addEventListener("click", () =>{
+        sidebar.classList.toggle("close");
+    })
+
+      
